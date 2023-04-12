66f658275f9ca78f9f138ae41ca3d7a0f7457ab1
sarvesh-masai <sarveshmsai@gmail.com>
2023-04-12 10:58:38 +0530

    Day-3

---
+++ While Loop/problem1.js
@@ -0,0 +1,6 @@
+//Print 0 to 10
+let i = 0;
+while(i<=10){
+  console.log(i);
+  i++
+}
---
+++ While Loop/problem2.js
@@ -0,0 +1,8 @@
+//Print odd number between 0 to 10 
+let i = 0;
+while(i<=10){
+  if (!(i%2==0)){
+   console.log(i); 
+  }
+  i++
+}
